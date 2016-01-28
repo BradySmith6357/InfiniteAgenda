53469b91630c7acfa86456cc60685ff0928b3e52
Student <student@RU-023.local>
2016-01-28 15:12:36 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var Monday = new Date(January 25, 2016);
+var Tuesday = new Date(January 26, 2016);
+var Wednesday = new Date(January 27, 2016);
+var Thursday = new Date(January 28, 2016);
+var Friday = new Date(January 29, 2016);
+var Saturday = new Date(January 30, 2016);
+var Sunday = new Date(January 31, 2016);
